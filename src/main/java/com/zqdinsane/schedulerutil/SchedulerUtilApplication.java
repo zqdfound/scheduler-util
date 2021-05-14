12dfc0f49a0e5167973390804b8c0c6320d15ea6
zhuangqd <1595051069@163.com>
2021-05-14 14:38:59 +0800

    Initial commit

---
+++ src/main/java/com/zqdinsane/schedulerutil/SchedulerUtilApplication.java
@@ -0,0 +1,13 @@
+package com.zqdinsane.schedulerutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerUtilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zqdinsane/schedulerutil/SchedulerUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zqdinsane.schedulerutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
